1db626db2a60b657f2c4b7d2e56b1d7a43b92c71
miniceno <1941439196@qq.com>
2019-06-12 17:28:14 +0800

    怎么回事，我明明踏上台阶了

---
+++ MatsuriBBSAndroid/app/src/main/java/com/example/matsuribbsandroid/fragment/MyFragment.java
@@ -0,0 +1,23 @@
+package com.example.matsuribbsandroid.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.matsuribbsandroid.R;
+
+public class MyFragment extends Fragment {
+    public MyFragment(){}
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_my,container,false);
+        return view;
+    }
+}
