b7f9df0a4a3b4900a06236ae48f6dd81cc60cbd6
miniceno <1941439196@qq.com>
2019-06-26 15:30:52 +0800

    发帖做好了

--- MatsuriBBSAndroid/app/src/main/java/com/example/matsuribbsandroid/ReleasePostActivity.java
+++ MatsuriBBSAndroid/app/src/main/java/com/example/matsuribbsandroid/ReleasePostActivity.java
@@ -3,12 +3,19 @@ package com.example.matsuribbsandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.Gravity;
+import android.widget.EditText;
 
 public class ReleasePostActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+
+
         setContentView(R.layout.activity_release_post);
     }
 }
--- MatsuriBBSAndroid/app/src/main/java/com/example/matsuribbsandroid/forumFragmentItem/SecondFragment.java
+++ MatsuriBBSAndroid/app/src/main/java/com/example/matsuribbsandroid/forumFragmentItem/SecondFragment.java
@@ -4,21 +4,39 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.codingending.popuplayout.PopupLayout;
 import com.example.matsuribbsandroid.R;
 //论坛界面的子Fragment
 public class SecondFragment extends Fragment {
     public SecondFragment(){}
-
+    private boolean useRadius=true;//是否使用圆角特性
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_second,container,false);
+
+       view.findViewById(R.id.btn_jianlai).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PopupLayout popupLayout= PopupLayout.init(getContext(), R.layout.more_reply);
+                popupLayout.setUseRadius(useRadius);
+                popupLayout.setDismissListener(new PopupLayout.DismissListener() {
+                    @Override
+                    public void onDismiss() {
+                        Toast.makeText(getContext(),"弹出窗口关闭",Toast.LENGTH_SHORT).show();
+                    }
+                });//添加监听器
+                popupLayout.show(PopupLayout.POSITION_BOTTOM);
+            }
+        });
+
         return view;
     }
 }
